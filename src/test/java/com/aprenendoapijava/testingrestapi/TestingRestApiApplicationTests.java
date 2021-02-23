c95dcd0c55be1a198980ffe4d03bf8eb72f1798d
Felipe <felipe.macieldsouza@gmail.com>
2021-02-23 10:49:52 -0300

    inicio projeto java springboot-maven-mysql

---
+++ src/test/java/com/aprenendoapijava/testingrestapi/TestingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprenendoapijava.testingrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
